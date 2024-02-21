844b7178224ab92dc250341dfbc87f28c0c781e1
frost1335 <dilrozbek.developer@gmail.com>
2024-02-21 17:08:54 +0500

    change: server hostname env variable

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,8 +4,7 @@ import LogoSecondary from './logo.svg'
 import UzIco from './uzbekistan.png'
 import RuIco from './russia.png'
 import EnIco from './united-kingdom.png'
-import CampaignLayout from './campaign-layout.webp'
 
 export {
-    LogoMain, LogoPrimary, LogoSecondary, UzIco, RuIco, EnIco, CampaignLayout
+    LogoMain, LogoPrimary, LogoSecondary, UzIco, RuIco, EnIco
 }
